c911bf108c32e271e696a81300e49e5fce2dab94
Manoela Cunha <manoela_fg@hotmail.com>
2021-10-28 13:13:49 -0300

    feat(providers): criação e configuração do providers

---
+++ src/providers/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import { CartProvider } from "./cart";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+const Providers = ({ children }: ProvidersProps) => {
+  return <CartProvider>{children}</CartProvider>;
+};
+
+export default Providers;
